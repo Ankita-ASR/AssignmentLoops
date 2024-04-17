b256e6e101f872868fa0d416ad03560e22aedfe2
Ankita <ankita7saha@gmail.com>
2024-04-17 22:16:50 +0600

    E6.1

--- src/Sum.java
+++ src/Sum.java
@@ -1,11 +1,22 @@
 public class Sum {
-    public static void main(String[] args){
-       int sum=0;
-    for(int i = 2; i<=100; i++){
-        if(i%2==0){
-       sum = sum+i;
-         }
-      }
-        System.out.printf("%d\n",sum);
-  }
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <= 100; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.printf("%d\n", sum);
+        /* The sum of all squares between 1 and 100 (inclusive).*/
+        int SquareSum = 0;
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if ((i * j) <=100) {
+                    SquareSum = SquareSum + i*j;
+                }
+            }
+        }
+        System.out.printf("%d\n", SquareSum);
+    }
 }
+
