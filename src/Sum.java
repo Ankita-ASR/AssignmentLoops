58e128f0d5bf97cb72aea307729635daa880caf4
Ankita <ankita7saha@gmail.com>
2024-04-17 20:34:52 +0600

    E6.1

---
+++ src/Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String[] args){
+       int sum=0;
+    for(int i = 2; i<=100; i++){
+        if(i%2==0){
+       sum = sum+i;
+         }
+      }
+        System.out.printf("%d\n",sum);
+  }
+}
